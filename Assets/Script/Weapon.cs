c3cb0c3d9442b41c25da1f8ad22392b65bf714a0
Joebigley <82520801+Joebigley@users.noreply.github.com>
2023-02-20 00:41:47 +0000

    began working on aim and shoot

---
+++ Assets/Script/Weapon.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+   public float offset;
+   public GameObject projectile;
+   public Transform shotPoint;
+   
+    private void Update()
+    {
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f,0f,rotZ);
+    
+        if(Input.GetMouseButtonDown(0))
+        {
+            Instantiate(projectile, shotPoint.position, transform.rotation);
+        }
+    
+    
+    
+    
+    } 
+
+
+}
+
