92f8544b6d02e07cd0ad671288cf23c3d3514559
Joebigley <Joe.wheatley989@gmail.com>
2023-03-12 00:23:39 +0000

    V2.9
    Added a basic health bar

--- Assets/Script/PlayerHealth.cs
+++ Assets/Script/PlayerHealth.cs
@@ -1,12 +1,16 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
 
-    public int maxHealth = 100;
-    public int health;
+    public float maxHealth = 100;
+    public float health;
+    public Image healthBar;
+    
+    
 
     // Start is called before the first frame update
     void Start()
@@ -14,13 +18,25 @@ public class PlayerHealth : MonoBehaviour
         health = maxHealth;
     }
 
+
+    private void Update()
+    {
+        healthBar.fillAmount = Mathf.Clamp(health / maxHealth, 0, 1);
+    }
+
+
     public void TakeDamage(int damage)
     {
         health -= damage;
+         
+
+        
+
         if (health < 0)
         {
             Destroy(gameObject);
 
         }
+        
     }
 }
